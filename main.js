7c374c200775f79720e9df817d77e5cd16bc740f
GABYZUL <a20210155@utem.edu.mx>
2022-05-31 19:58:58 -0500

    intro

---
+++ main.js
@@ -0,0 +1,20 @@
+//imprimir por console
+// alert("Hola desde archivo externo");
+// console.log("Hola desde consola");
+// console.log(5+3)
+// console.log(4>3)
+
+//Document write
+// document.write("Hola desde document.write");
+// //Imorimir mediante una alerta emergente
+// alert("Hola desde alert");
+
+
+// variables
+var nombreUsuario = prompt("Ingrese su nombre")
+// Pedir datos al usuario
+
+document.write("Hola "+nombreUsuario);
+// ESTO SOLO SE VE EN CONSOLA
+ console.log("Hola, como estas "+ nombreUsuario)
+
